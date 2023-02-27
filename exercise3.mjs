a33adf3237f19d39070996d313a689d959758568
alafSantos <alaf.ns4@gmail.com>
2023-02-27 15:19:27 +0100

    exercise3 comments

--- exercise3.mjs
+++ exercise3.mjs
@@ -7,28 +7,47 @@ export class Std {
      * @param {*} firstName 
      * @param {*} id 
      */
-    constructor(lastName, firstName, id){
+    constructor(lastName, firstName, id) {
         this.lastName = lastName;
         this.firstName = firstName;
         this.id = id;
     }
 
-    toString(){
-        return "student: " + 
+    /**
+     * 
+     * @returns a string of characters built from the properties of the object
+     */
+    toString() {
+        return "student: " +
             this.lastName.toString() + ", " +
             this.firstName.toString() + ", " +
             this.id.toString();
     }
 }
 
+/**
+ * class ForeignStud extending Std that also allows to give a nationality to a student, 
+ * so the arguments of the constructor are lastName, firstName, id and nationality.
+ */
 export class ForeignStud extends Std {
 
-    constructor(lastName, firstName, id, nationality){
+    /**
+     * 
+     * @param {*} lastName 
+     * @param {*} firstName 
+     * @param {*} id 
+     * @param {*} nationality 
+     */
+    constructor(lastName, firstName, id, nationality) {
         super(lastName, firstName, id);
         this.nationality = nationality;
     }
 
-    toString(){
+    /**
+     * 
+     * @returns a string of characters built from the properties of the object
+     */
+    toString() {
         return super.toString() + ", " + this.nationality.toString();
     }
 
