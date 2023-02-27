4495c18bdf1b9e8eaf53218b0536f38019718b7a
alafSantos <alaf.ns4@gmail.com>
2023-02-27 15:16:15 +0100

    exercise3 - OK

--- exercise3.mjs
+++ exercise3.mjs
@@ -1,5 +1,35 @@
 "use strict";
 
-class Std {
+export class Std {
+    /**
+     * 
+     * @param {*} lastName 
+     * @param {*} firstName 
+     * @param {*} id 
+     */
+    constructor(lastName, firstName, id){
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.id = id;
+    }
+
+    toString(){
+        return "student: " + 
+            this.lastName.toString() + ", " +
+            this.firstName.toString() + ", " +
+            this.id.toString();
+    }
+}
+
+export class ForeignStud extends Std {
+
+    constructor(lastName, firstName, id, nationality){
+        super(lastName, firstName, id);
+        this.nationality = nationality;
+    }
+
+    toString(){
+        return super.toString() + ", " + this.nationality.toString();
+    }
 
 }
--- test_exercise3.mjs
+++ test_exercise3.mjs
@@ -1,1 +1,9 @@
 "use strict";
+
+import {Std, ForeignStud} from "./exercise3.mjs";
+
+var student = new Std("Dupond", "John", 1835);
+console.log(student.toString());
+
+var student2 = new ForeignStud("Doe", "John", "432", "American");
+console.log(student2.toString());
