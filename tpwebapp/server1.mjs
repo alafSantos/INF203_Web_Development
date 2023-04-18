565c62149b09e7ac08a2198db5d4d72f9599d20f
Alaf Do Nascimento Santos <asantos-22@tp-3a101-02.enst.fr>
2023-04-18 13:03:29 +0200

    Question 0 - tpwebapp

--- tpwebapp/server1.mjs
+++ tpwebapp/server1.mjs
@@ -2,25 +2,113 @@
 
 import { createServer } from "http";
 import { argv } from "node:process";
-// import {fs} from "node:fs";
+import { extname, join } from "node:path";
+import { createReadStream, statSync } from "node:fs";
+
+const users = new Set();
+
+function createPage(message) {
+  const html =
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head></head>
+        <body>
+          ${message}
+        </body>
+      </html>
+    `;
+  return html;
+}
 
 // process requests
 function webserver(request, response) {
-    let url = request.url;
+  const { url, method } = request;
 
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
-    if (url == "/end") {
-        response.end("<!doctype html><html><body>The server will stop now.</body></html>");
-        process.exit(0);
+  // process GET requests to /files
+  if (method === "GET" && url.startsWith("/files")) {
+    if (url.includes("..")) {
+      response.setHeader("Content-Type", "text/plain; charset=utf-8");
+      response.writeHead(403);
+      response.end("Forbidden URL.");
     }
     else {
-        response.end("<!doctype html><html><body>Server works.</body></html>");
+      const mimeTypes = {
+        ".html": "text/html",
+        ".css": "text/css",
+        ".js": "application/javascript",
+        ".mjs": "application/javascript",
+        ".png": "image/png",
+        ".jpg": "image/jpeg",
+        ".gif": "image/gif",
+      };
+
+      // remove "/files/" prefix from URL
+      let path = url.slice(7);
+      // construct file path
+      let filePath = join(".", path);
+
+      try {
+        // check if file exists and is a file (not a directory)
+        let fileStats = statSync(filePath);
+        if (fileStats.isFile()) {
+          // get file extension to determine MIME type
+          let fileExt = extname(filePath);
+          let mimeType = mimeTypes[fileExt];
+
+          // set response headers
+          response.setHeader("Content-Type", mimeType);
+          response.setHeader("Content-Length", fileStats.size);
+
+          // stream file contents to response
+          let fileStream = createReadStream(filePath);
+          fileStream.pipe(response);
+
+          return;
+        }
+      } catch (error) {
+        // file doesn't exist or isn't a file
+        console.error(error);
+      }
+      // return 404 error if file not found or isn't a file
+      response.setHeader("Content-Type", "text/plain; charset=utf-8");
+      response.writeHead(404);
+      response.end("File not found or is a directory.");
     }
+  }
+  // process GET requests to /end
+  else if (method === "GET" && url == "/stop") {
+    // generate HTML response
+    let message = "The server will stop now.";
+    const html = createPage(message);
+
+    // send response 
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    response.writeHead(200);
+    response.end(html);
+    process.exit(0);
+  }
+  // anything else
+  else {
+    // generate HTML response
+    let message = "Server works.";
+    const html = createPage(message);
+
+    // send response 
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    response.writeHead(200);
+    response.end(html);
+  }
 }
 
 // server instanciation
 const server = createServer(webserver);
 
 // server starting
-let port = argv[2];
-server.listen(port, (err) => { });
+let port = argv[2] || 8000;
+server.listen(port, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`Server running on port ${port}`);
+  }
+});
