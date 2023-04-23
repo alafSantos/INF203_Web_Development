8f2fe50eb07c04f7e0cb8cedec3b883dd520d44b
Alaf Do Nascimento Santos <asantos-22@tp-3b01-09.enst.fr>
2023-04-23 19:44:35 +0200

    12/14

--- tpserver2/server2.mjs
+++ tpserver2/server2.mjs
@@ -27,11 +27,11 @@ function createPage(message) {
 
 // process requests
 function webserver(request, response) {
+    const database_name = "db.json";
     function loadDB() {
         try {
-            const rawData = fs.readFileSync('db.json');
+            const rawData = fs.readFileSync(database_name);
             dbData = JSON.parse(rawData);
-            console.log('db.json loaded');
         } catch (err) {
             console.error(`Failed to load db.json: ${err}`);
         }
@@ -71,13 +71,100 @@ function webserver(request, response) {
         response.end(number);
     }
     else if (method == "GET" && url.startsWith("/byauthor")) {
-        const authorName = url.slice(11); // remove '/byauthor/'
         loadDB();
+        const authorName = url.slice(11); // remove '/byauthor/'
         const count = dbData.filter(paper => paper.authors.some(author => author.toLowerCase().includes(authorName.toLowerCase()))).length + "";
         response.setHeader("Content-Type", "text/plain; charset=utf-8");
         response.writeHead(200);
         response.end(count);
 
+    }
+    else if (method == "GET" && url.startsWith("/descriptors")) {
+        loadDB();
+        const authorName = url.slice(13); // remove '/descriptors/'
+        const papers = dbData.filter(paper => paper.authors.some(author => author.toLowerCase().includes(authorName.toLowerCase())));
+        const descriptors = papers.map(paper => {
+            return {
+                key: paper.key,
+                title: paper.title,
+                journal: paper.journal,
+                year: paper.year,
+                month: paper.month,
+                keywords: paper.keywords,
+                lang: paper.lang,
+                authors: paper.authors,
+                category: paper.category,
+                state: paper.state,
+                dept: paper.dept,
+                group: paper.group
+            };
+        });
+        const json = JSON.stringify(descriptors);
+        response.setHeader("Content-Type", "application/json; charset=utf-8");
+        response.writeHead(200);
+        response.end(json);
+
+    }
+    else if (method == "GET" && url.startsWith("/titlelist")) {
+        loadDB();
+        const authorName = url.slice(11).toLowerCase(); // remove '/titlelist/' and convert to lowercase
+        const papers = dbData.filter(paper => paper.authors.some(author => author.toLowerCase().includes(authorName)));
+        const titles = papers.map(paper => paper.title);
+        const json = JSON.stringify(titles);
+        response.setHeader("Content-Type", "application/json; charset=utf-8");
+        response.writeHead(200);
+        response.end(json);
+    }
+    else if (method == "GET" && url.startsWith("/publication")) {
+        loadDB();
+        const key = url.slice(13); // remove '/publication/'
+        const publication = dbData.find(paper => paper.key === key);
+
+        if (publication) {
+            const descriptor = {
+                key: publication.key,
+                title: publication.title,
+                journal: publication.journal,
+                year: publication.year,
+                month: publication.month,
+                keywords: publication.keywords,
+                lang: publication.lang,
+                authors: publication.authors,
+                category: publication.category,
+                state: publication.state,
+                dept: publication.dept,
+                group: publication.group
+            };
+
+            const json = JSON.stringify(descriptor);
+            response.setHeader("Content-Type", "application/json; charset=utf-8");
+            response.writeHead(200);
+            response.end(json);
+
+        } else {
+            // if publication not found, send a 404 error response
+            response.writeHead(404);
+            response.end("Publication not found");
+        }
+    }
+    else if (method == "DELETE" && url.startsWith("/publication")) {
+        loadDB();
+        const key = url.slice(13); // remove '/publication/'
+        const publication = dbData.findIndex(paper => paper.key === key);
+
+        if (publication >= 0) {
+            dbData.splice(publication, 1);
+            let json_str = JSON.stringify(dbData);
+            fs.writeFileSync(database_name, json_str);
+            response.setHeader("Content-Type", "application/json; charset=utf-8");
+            response.writeHeader(200);
+            response.end(json_str);
+
+        } else {
+            // if publication not found, send a 404 error response
+            response.writeHead(404);
+            response.end("Publication not found");
+        }
     }
     // anything else
     else {
